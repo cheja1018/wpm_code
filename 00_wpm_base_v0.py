0c560d0196d89ce157f2a8396ee7ffd29bff7cc9
cheja1018 <72240876+cheja1018@users.noreply.github.com>
2023-06-07 09:30:50 +1200

    00_wpm_base_v0
    comments

---
+++ 00_wpm_base_v0.py
@@ -0,0 +1,15 @@
+"""
+base component for my wpm calculator
+V0 - skeleton setup
+author - Jasmine Chen
+2023
+"""
+
+# import libraries below this line **********************
+
+
+# all functions below this line *************************
+
+
+# main routine below this line **************************
+print("Hello")
